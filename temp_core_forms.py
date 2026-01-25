1f3f82b8caa9a61365007db268b4c06b2079f902
fabianopolone <fabianopolone@gmail.com>
2026-01-25 14:30:34 -0300

    Correcao erro 50 - 02

---
+++ temp_core_forms.py
@@ -0,0 +1,6 @@
+﻿with open('core/forms.py') as f:
+    for i,line in enumerate(f,1):
+        if 'AdventureLoginForm' in line or 'clean_whatsapp_number' in line:
+            print(i, line.strip())
+        if 'normalize_whatsapp_number' in line:
+            print(i, line.strip())
---
+++ temp_info.py
@@ -0,0 +1,7 @@
+﻿import itertools
+with open('accounts/models.py') as f:
+    for i,line in enumerate(f,1):
+        if 'normalize_whatsapp_number' in line:
+            print(i, line.strip())
+        if 'USERNAME_FIELD' in line:
+            print(i, line.strip())
